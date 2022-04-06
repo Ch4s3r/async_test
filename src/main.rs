560c8273ea3636b22ddb82e826f15736fada1137
Ch4s3r <lamprecht.patrick1@gmail.com>
2022-04-06 22:22:59 +0200

    Parallel execution achieved

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use futures::{stream, StreamExt};
+use par_stream::prelude::*;
 use tokio::time::{Duration, sleep};
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    stream::iter(1..10).then(|i| async move {
+    stream::iter(1..10).par_then(None, |i| async move {
         wait_and_print(i).await
     }).collect::<Vec<_>>().await;
 }
 
 async fn wait_and_print(i: u32) {
-    sleep(Duration::from_millis(333)).await;
+    sleep(Duration::from_millis(3000)).await;
     println!("waiting 1 second, iteration {i}");
 }
